555dc06673fceae0057518425c419a573f8d16a1
Samiuddin <mdsamiuddin2005@outlook.com>
2024-09-12 23:05:09 +0530

    added changes , backend for authentication completed !!

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -11,18 +11,10 @@ const Navbar = () => {
     return (
         <>
         <nav className="navbar">
-            <Link to="/home" className='flex-none w-10 mb-[-20px]' >
+            <Link to="/" className='flex-none w-10 mb-[-20px]' >
             <img src={logo} alt="a logo" /> 
             <button className='hide bg-red'>cool</button>
             </Link>
-        {/* {open && <div className='py-4 px-[5vh] absolute w-full md:show left-0 top-full mt-0.5 border-b border-grey md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto' >
-                <input
-                    type="text"
-                    placeholder='Search'
-                    className='w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12'
-                    />
-                    <i className="fi fi-rr-search-heart absolute right-[10%] top-1/2 -translate-y-1/2 md:pointer-events-none  md:left-5 text-xl text-dark-grey "></i>
-            </div>} */}
             <div className={`${open ? "show" : "hide"} py-4 px-[5vh] absolute w-full md:show left-0 top-full mt-0.5 border-b border-grey md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto`} >
                 <input
                     type="text"
--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 import express from "express"
 import mongoose from "mongoose";
 import 'dotenv/config'
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { nanoid } from "nanoid";
+
+// schema imported here
+import User from "./Schema/User.js"
+
 
 
 const app = express()
-
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 let PORT = 3000;
 
 
@@ -21,11 +29,91 @@ app.get("/" , (req ,res) => {
 
     res.send("Hello World") }
 )
+
+const generateUsername = async (email) => {
+    let username = email.split("@")[0]
+    let isUsernameNotUnique = await User.exists({ "personal_info.username" : username }).then((result) =>  result)
+
+    isUsernameNotUnique ? username += nanoid(3) : "" ;
+    return username
+               
+}
+
+const formatDatatoSend = (user) => {
+
+    const access_token = jwt.sign({ id : user._id} , process.env.SECRET_ACCESS_KEY )
+    console.log(access_token)
+    return {
+        access_token : access_token,
+        profile_img : user.personal_info.profile_img,
+        fullname : user.personal_info.fullname,
+        username : user.personal_info.username,
+    }
+}
+
 app.post("/signup" , (req ,res) => {
-    res.json(req.body)
-    console.log("some data has been send ")
+    const {fullname , email , password} = req.body
+    console.log(req.body)
+    // validating the data from frontend 
+    if(fullname.length < 3){
+        return res.status(403).json({"error" : "Fullname must be at least 3 characters long"})
+    }
+    if(!email.length){
+        return res.status(403).json({"error" : "Email is required"})
+    }
+    if(!password.length){
+        return res.status(403).json({"error" : "Password is required"})
+    }
+    if(!emailRegex.test(email)){
+        return res.status(403).json({"error" : "Email is not valid"})
+    }
+    if(!passwordRegex.test(password)) {
+        return res.status(403).json({"error" : "Password should be 6 to 20 characters long with a numeric , 1 lowercase and 1 uppercase letter"})
+    }
+    bcrypt.hash(password , 10 , async (err , hashpasword)=>{
+        const username = await  generateUsername(email) 
+
+        let user = new User({
+            personal_info : {fullname , email , password : hashpasword , username }
+        })
+        user.save().then((user) => {
+            return res.status(200).json(formatDatatoSend(user))
+            // console.log(FormatDatatoSend(user))
+        }).catch((err) => {
+            if(err.code = 11000) {
+                return res.status(403).json({"error" : "Email already exists"})
+            }
+            res.status(500).json({"error" : "Something went wrong"} )
+        })
+    })    
+    
 })
 
+app.post("/signin" , (req , res) => {
+    const {email , password} = req.body
+    User.findOne({ "personal_info.email" : email }).
+    then((user) => {
+        if (!user) {
+            return res.status(403).json({"error" : "Invalid credentials"})
+        }
+        console.log(user)
+        bcrypt.compare(password , user.personal_info.password , (err , result) => {
+            if(err) {
+                return res.status(403).json({"error" : "Invalid credentials"})
+            }else {
+                return res.status(200).json(formatDatatoSend(user))
+            }
+        })
+
+        // return res.json({"status" : "got user Document "})
+    }).
+    catch(err=> {
+        console.log(err.message)
+        return res.status(500).json({"error" : err.message})
+    })
+} )
+
+              
 app.listen(PORT , () => {
     console.log("Server is running on port " + PORT)
 })
