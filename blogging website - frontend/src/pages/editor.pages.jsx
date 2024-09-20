c6a09a9d9ed02a9d6cab2ef5d21df42b6f53e1cc
Samiuddin <mdsamiuddin2005@outlook.com>
2024-09-20 21:31:48 +0530

    little work done only

---
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { UserContext } from "../App";
+import { Navigate } from "react-router-dom";
+
+const Editor = () => {
+    let { userAuth : { access_token }  } = useContext(UserContext)
+
+    return (
+        access_token === null ? <Navigate to="/signin"/> : 
+        <h1>u can access the editor page </h1>
+    )
+}
+
+export default Editor;  
