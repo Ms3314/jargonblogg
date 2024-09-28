584273801aefd4a4bc7784ceb7a40fb925d6cdfd
Samiuddin <mdsamiuddin2005@outlook.com>
2024-09-28 22:55:58 +0530

    blog editor and the blog banner is done

--- blogging website - frontend/src/components/tools.component.jsx
+++ blogging website - frontend/src/components/tools.component.jsx
@@ -7,39 +7,65 @@ import Header from "@editorjs/header"
 import Quote from "@editorjs/quote"
 import Marker from "@editorjs/marker"
 import InlineCode from "@editorjs/inline-code"
+import { uploadImage } from "../common/aws"
 
-const uploadImageByUrl = () {
-    
-}
+const uploadImageByUrl = (e) => {
+    let link = new Promise((resolve, reject) => {
+        try {
+            resolve(e); // e should be a valid image URL
+        } catch (error) {
+            reject(error);
+        }
+    });
 
+    return link.then(url => {
+        return {
+            success: 1,
+            file: { url }
+        };
+    });
 
-export const tools = {
-    embed : Embed ,
-    list : {
-        class : List , 
-        inlineToolbar : true
-    } ,
-    image : {
-        class : Image , 
-        config : {
-            uploader : {
-                uploadByUrl :  , 
-                uploadByFile : 
+};
+
+const uploadImageByFile =(e) => {
+    return uploadImage(e).then(url => {
+        if(url) {
+            return {
+                success : 1 , 
+                file : {url}
             }
         }
-    } , 
-    header : {
-        class : Header , 
-        config : {
-            placeholder : "Type Heading...",
-            levels : [2 , 3],
-            defaultLevel:2
-        }
-    } , 
-    quote : {
-        class : Quote , 
-        inlineToolBar : true
-    } , 
-    maker : Marker , 
-    inlineCode : InlineCode 
+    })
 }
+
+export const tools = {
+    embed: Embed,
+    list: {
+        class: List,
+        inlineToolbar: true,
+    },
+    image: {
+        class: Image,
+        config: {
+            uploader: {
+                uploadByUrl: uploadImageByUrl,
+                uploadByFile: uploadImageByFile,  // if needed
+            }
+        }
+    },
+    header: {
+        class: Header,
+        config: {
+            placeholder: "Type Heading...",
+            levels: [2, 3],
+            defaultLevel: 2,
+        }
+    },
+    quote: {
+        class: Quote,
+        inlineToolbar: true,  // Fixed typo (was inlineToolBar)
+    },
+    marker: Marker,  // Fixed casing (was 'maker')
+    inlineCode: InlineCode
+};
+
