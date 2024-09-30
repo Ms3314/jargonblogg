443282b56ae5b9b6bd2f721dee8220d9f0f699d7
Samiuddin <mdsamiuddin2005@outlook.com>
2024-10-01 00:01:56 +0530

    work in progress related to the home page

--- blogging website - frontend/src/components/inpage-navigation.component.jsx
+++ blogging website - frontend/src/components/inpage-navigation.component.jsx
@@ -1,21 +1,39 @@
 import { useScroll } from "framer-motion";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const InPageNavigation = ({ routes }) => {
+const InPageNavigation = ({ routes , defaultActiveIndx = 0 }) => {
     let activeTabLineRef = useRef();
-    let [inPageNavIndex , setInPageNavIndex] = useState(0);
+    let activeTabRef = useRef()
+    let [inPageNavIndex , setInPageNavIndex] = useState(defaultActiveIndx);
+    const changePageState =(btn , i)=> {
+        let {offsetWidth , offsetLeft} = btn 
+        activeTabLineRef.current.style.width = offsetWidth + "px"
+        activeTabLineRef.current.style.left = offsetLeft + "px"
+        setInPageNavIndex(i)
+    }
+
+    useEffect(()=> {
+      changePageState(activeTabRef.current , defaultActiveIndx)  
+    },[])
+
     return (
         <>
             <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
                         {
                             routes.map((route , i ) => {
                                 return (
-                                    <button key={i} className={"p-4 px-5 capitalize " + (inPageNavIndex == i ? "text-dark-grey" : "text-black" )}>
+                                    <button key={i} className={"p-4 px-5 capitalize " + (inPageNavIndex == i ? "text-black" : "text-dark-grey" )}
+                                    ref={ i == defaultActiveIndx ? activeTabRef : null }
+                                    onClick={(e)=>{
+                                        changePageState(e.target , i)
+                                    }}
+                                    >
                                         {route}
                                     </button>
                                 )
                             })
                         }
+                        <hr ref={activeTabLineRef} className="absolute bottom-0 duration-300 "/>
             </div>
         </>
     )
