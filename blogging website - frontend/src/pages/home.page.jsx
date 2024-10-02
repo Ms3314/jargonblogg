1d0b9cb5a749202365ce7151ec87e006e2ef3002
Samiuddin <mdsamiuddin2005@outlook.com>
2024-10-02 15:14:01 +0530

    inPageNavigation is Done

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -7,8 +7,13 @@ const HomePage = () => {
             <section className="h-cover flex justify-center gap-10 ">
                 {/* this div will be for the latest blogs */}
                 <div className="w-full">
-                    <InPageNavigation routes={["home" , "trending blogs"]}>
-                        
+                    {/* the routes will be displayed on screen , and on the other hand the defaultHidden is which will be hidden in larger screens */}
+                    <InPageNavigation routes={["home" , "trending blogs"]} defaultHidden={["trending blogs"]}>
+                       
+                        <h1>Latest Blogs lol hahaha </h1>
+
+                        <h1>Trending Blogs here </h1>
+
                     </InPageNavigation>
                 </div>
 
