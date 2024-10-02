86606ef4b5b3e0560fbcf42ff3e4106b1419795d
Samiuddin <mdsamiuddin2005@outlook.com>
2024-10-02 19:35:25 +0530

    home page done , the blogs are also being displayed

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,7 +1,27 @@
 import AnimationWraper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import axios from "axios"
+import Loader from "../components/loader.component"
+import BlogPostCard from "../components/blog-post.component"
+import {useEffect , useState} from "react"
 
 const HomePage = () => {
+    let [blog , setBlog] = useState(null)
+
+    const fetchLatestBlogs = () => {
+        axios.get(import.meta.env.VITE_SERVER_DOMAIN + '/latest-blogs')
+        .then(({ data })  => {
+            setBlog(data.blogs)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        fetchLatestBlogs()
+    } , [])
+
     return (
         <AnimationWraper>
             <section className="h-cover flex justify-center gap-10 ">
@@ -10,7 +30,16 @@ const HomePage = () => {
                     {/* the routes will be displayed on screen , and on the other hand the defaultHidden is which will be hidden in larger screens */}
                     <InPageNavigation routes={["home" , "trending blogs"]} defaultHidden={["trending blogs"]}>
                        
-                        <h1>Latest Blogs lol hahaha </h1>
+                        <>
+                            {
+                                blog == null ? <Loader /> :
+                                blog.map((blog , i) => {
+                                    return <AnimationWraper transition={{duration : 1 , delay : i*.1}} key={i}>
+                                        <BlogPostCard content={blog} author={blog.author.personal_info}/>
+                                    </AnimationWraper>
+                                })  
+                            }
+                        </>                
 
                         <h1>Trending Blogs here </h1>
 
