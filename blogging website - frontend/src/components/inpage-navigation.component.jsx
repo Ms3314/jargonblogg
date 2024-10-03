de7bdf66e8fcd8af79310367e93441a7a236cc3b
Samiuddin <mdsamiuddin2005@outlook.com>
2024-10-03 23:45:49 +0530

    the basic homepage is doneee and also the cards or tags is also done : one problem is that when i am clicking on the tag that is already existing in the db its not like clicking check for that error

--- blogging website - frontend/src/components/inpage-navigation.component.jsx
+++ blogging website - frontend/src/components/inpage-navigation.component.jsx
@@ -1,9 +1,12 @@
 import { useScroll } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import React from "react"
+export let activeTabLineRef ;
+export let activeTabRef ; 
+
 const InPageNavigation = ({ routes , defaultActiveIndx = 0 , defaultHidden , children}) => {
-    let activeTabLineRef = useRef();
-    let activeTabRef = useRef()
+    activeTabLineRef = useRef();
+    activeTabRef = useRef()
     let [inPageNavIndex , setInPageNavIndex] = useState(defaultActiveIndx);
     
     const changePageState =(btn , i)=> {
@@ -21,10 +24,11 @@ const InPageNavigation = ({ routes , defaultActiveIndx = 0 , defaultHidden , chi
         <>
            <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
                     {routes.map((route, i) => (
-                            <React.Fragment key={i}>
+                        <>
                                 <button
                                     className={`p-4 px-5 capitalize ${inPageNavIndex === i ? "text-black" : "text-dark-grey"} ${defaultHidden.includes(route) ? "md:hidden" : ""}`}
                                     ref={i === defaultActiveIndx ? activeTabRef : null}
+                                    key={i}
                                     onClick={(e) => {
                                     changePageState(e.target, i);
                                     }}
@@ -32,12 +36,14 @@ const InPageNavigation = ({ routes , defaultActiveIndx = 0 , defaultHidden , chi
                                     {route}
                                 </button>
                                 <hr
-                                    ref={activeTabLineRef}
-                                    className={`absolute bottom-0 duration-300 ${ defaultActiveIndx == 0 ? " md:hidden" : ""}`}
-                                />
-                            </React.Fragment>
+                                ref={activeTabLineRef}
+                                className={`absolute bottom-0 duration-300 `}
+                            />
+                            </>
+                                
                     ))}
-</div>
+        </div>
+                                
 
             {/* isArray is methood used to check if the given item is an array or not   */}
     {/* ao basically when we pass the children normally it does not act as an array but if we pass more than one children it will act as an array to becomes in a form of Array*/}
--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -5,10 +5,14 @@ import Loader from "../components/loader.component"
 import BlogPostCard from "../components/blog-post.component"
 import MinimalBlogPost from "../components/nobanner-blog-post.component"
 import {useEffect , useState} from "react"
+import  {activeTabRef }  from "../components/inpage-navigation.component";
+
 
 const HomePage = () => {
     let [blog , setBlog] = useState(null)
     let [trendingBlog , setTrendingBlog] = useState(null)
+    let [pageState , setPageState] = useState("home")
+    let categories = ["programming" , 'lorem' , "chicken" , "food" , "design" , "tech" , "jijaji" , "CSI" , "marketting" ]
 
     const fetchLatestBlogs = () => {
         axios.get(import.meta.env.VITE_SERVER_DOMAIN + '/latest-blogs')
@@ -30,10 +34,29 @@ const HomePage = () => {
         })
     }
 
+    const loadBlogByCategory = (e) => {
+        let category = e.target.innerText.toLowerCase() ; 
+        // agar tum blog ko null kar diye koyi bhi blog display nai hone wale hai 
+        setBlog(null);
+        if(pageState == category) {
+            // this implies if u clicked the button onces again just go back to the home page or just set it back to home ok
+            setPageState("home");
+            return ; 
+        }
+        setPageState(category)
+    }   
+
     useEffect(() => {
+        activeTabRef.current.click()
+
+        if(pageState == "home") {
         fetchLatestBlogs();
+        }
+        if(!trendingBlog) {
         fetchTrendingBlogs()
-    } , [])
+        }
+
+    } , [pageState])
 
     return (
         <AnimationWraper>
@@ -41,7 +64,7 @@ const HomePage = () => {
                 {/* this div will be for the latest blogs */}
                 <div className="w-full">
                     {/* the routes will be displayed on screen , and on the other hand the defaultHidden is which will be hidden in larger screens */}
-                    <InPageNavigation routes={["home" , "trending blogs"]} defaultHidden={["trending blogs"]}>
+                    <InPageNavigation routes={[pageState , "trending blogs"]} defaultHidden={["trending blogs"]}>
                        
                         <>
                             {
@@ -55,7 +78,7 @@ const HomePage = () => {
                         </>                
 
                         <>
-                        {
+                            {
                                 trendingBlog == null ? <Loader /> :
                                 trendingBlog.map((blog , i) => {
                                     return <AnimationWraper transition={{duration : 1 , delay : i*.1}} key={i}>
@@ -69,6 +92,41 @@ const HomePage = () => {
                 </div>
 
                 {/* filters and trending blogs */}
+                {/* max-md means from smaller screens to medium screens  */}
+                            <div className="min-w-[40%] lg:min-[400px] max-w-min border-2 border-grey pl-8 pt-10 mt-[-20px] max-md:hidden">
+                                <div className="flex flex-col gap-10">
+                                {/* this is the main div in which we have trensing and the isk those buttons  */}
+                                
+                                {/* here we have those category buttons */}
+                                <div>
+                                    <h1 className="font-medium text-xl mb-8 ">Stories from all intrests</h1>
+                                    
+                                    <div className="flex gap-3 flex-wrap ">
+                                        {
+                                            categories.map((category , i)=> {
+                                                return <button onClick={loadBlogByCategory} className={'tag ' + ( pageState == category ? "bg-black text-white" : "") } key={i} >{category}</button>
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                                {/* here we have the trending blogs */}
+                                <div>
+                                    {/* trending blog part , fetching the data from backend  */}
+                                    <h1 className="font-medium text-xl mb-8 ">Trending <i class="fi fi-rr-arrow-trend-up"></i></h1>
+                                    
+                                    {
+                                        trendingBlog == null ? <Loader /> :
+                                        trendingBlog.map((blog , i) => {
+                                            return <AnimationWraper transition={{duration : 1 , delay : i*.1}} key={i}>
+                                                <MinimalBlogPost blog={blog} index={i}/>
+                                            </AnimationWraper>
+                                        })  
+                                    }
+                                </div>
+
+                                </div>
+                            </div>
+
                 <div>
                      
                 </div>
